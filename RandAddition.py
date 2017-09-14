8095c1fa5f58b6f2b72330fa48b4762fd0a43f77
desslinga <fides.linga@mail.utoronto.ca>
2017-09-14 10:34:18 -0400

    Printing current value of result on each iteration

--- RandAddition.py
+++ RandAddition.py
@@ -4,6 +4,7 @@ result = 0
 
 while count_var < 10:
 	result += random.randint(1,10)
+	print("Current result is " + str(result))
 	count_var += 1
 
 print("Adding 10 random integers ranging 1-10", result)
